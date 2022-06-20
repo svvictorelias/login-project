b05ac24ccfcee8ccb8233cbc12c0ef4a192bcad2
Victor Elias <svvictorelias@gmail.com>
2022-06-20 09:29:08 -0300

    testing deploy

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import Input from "../../components/Input/index.jsx";
-import Button from "../../components/Button";
+import Input from "../../components/Input/index";
+import Button from "../../components/Button/index";
 import * as C from "./styles";
 import { Link, useNavigate } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
